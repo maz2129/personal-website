d97a3ecbe746ad1ce226831ccd2ce1dfa22211d1
Matt Zinman <maz2129@columbia.edu>
2023-03-26 19:50:11 -0400

    Updates

---
+++ server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
+export const file = '_app/immutable/entry/error.svelte.31d20d90.js';
+export const imports = ["_app/immutable/entry/error.svelte.31d20d90.js","_app/immutable/chunks/index.898f3223.js","_app/immutable/chunks/singletons.d29083b7.js"];
+export const stylesheets = [];
+export const fonts = [];
