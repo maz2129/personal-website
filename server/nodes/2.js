ec11040f926495924dfb0227d03e41598a8ebe10
Matt Zinman <maz2129@columbia.edu>
2023-03-27 17:10:09 -0400

    Updates

--- server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const file = '_app/immutable/entry/_page.svelte.2df32e92.js';
-export const imports = ["_app/immutable/entry/_page.svelte.2df32e92.js","_app/immutable/chunks/index.898f3223.js"];
-export const stylesheets = ["_app/immutable/assets/_page.e29fdc13.css"];
-export const fonts = [];
