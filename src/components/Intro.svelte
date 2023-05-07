ae33d810d1acaa791a79db28d65de8a83eeff7e8
Matt Zinman <maz2129@columbia.edu>
2023-05-07 14:25:41 -0400

    update bio?

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,5 +1,5 @@
 <h2>Matt Zinman</h2>
-<p>Hi 👋. I'm interested in distributed systems, data journalism, and civic technology. Currently I work as a software engineer at Peloton where I build, design, and maintain the backend of the in-class leaderboard. You can reach me on <a target="_blank" href="https://www.linkedin.com/in/matt-zinman-480315153/">LinkedIn</a> or by email at mattalexzin@gmail.com.</p>
+<p>Hi 👋. I'm interested in distributed systems, data journalism, and civic technology. Currently I work as a software engineer at Peloton where I build, design, and maintain Peloton's highly scalable metrics ingestion pipeline and related services like the in-class leaderboard. You can reach me on <a target="_blank" href="https://www.linkedin.com/in/matt-zinman-480315153/">LinkedIn</a> or by email at mattalexzin@gmail.com.</p>
 
 <style>
     p {
